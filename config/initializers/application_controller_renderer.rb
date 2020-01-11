3f8134d54eef6a69ec9600a71946f5a30cf94506
坂本裕樹 <hiroki@Hiroki-Macbook.local>
2020-01-11 17:14:46 +0900

    [Finish] 1-7

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+Refile.secret_key = 'b8d373bad96a7c4d788b4fa445b80eb903a038196438643add34a7dcb0c0e67a55fa1cea53f9268312866132405e17702fa8557de7caa91fd79ad23b80e91362'
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
